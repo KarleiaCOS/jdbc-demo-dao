e3409b24c9d47946029cb0fc3bde47a1f48327fc
Karleia <karleia.c.o.s@gmail.com>
2022-12-22 15:03:12 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -53,16 +53,8 @@ public class SellerDaoJDBC implements SellerDao{
 			ps.setInt(1, id);
 			rs = ps.executeQuery();
 			if(rs.next()) {
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getNString("DepName"));
-				Seller objSeller = new Seller(); 
-				objSeller.setId(rs.getInt("Id"));
-				objSeller.setName(rs.getString("Name"));
-				objSeller.setEmail(rs.getString("Email"));
-				objSeller.setbaseSalary(rs.getDouble("BaseSalary"));
-				objSeller.setBirthDate(rs.getDate("BirthDate"));
-				objSeller.setDepartment(dep);
+				Department dep = instantiateDepartment(rs);
+				Seller objSeller = instantiateSeller(rs, dep);
 				return objSeller;
 			}
 			
@@ -76,6 +68,24 @@ public class SellerDaoJDBC implements SellerDao{
 			DB.closeResultSet(rs);
 		}
 		
+	}
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller objSeller = new Seller(); 
+		objSeller.setId(rs.getInt("Id"));
+		objSeller.setName(rs.getString("Name"));
+		objSeller.setEmail(rs.getString("Email"));
+		objSeller.setbaseSalary(rs.getDouble("BaseSalary"));
+		objSeller.setBirthDate(rs.getDate("BirthDate"));
+		objSeller.setDepartment(dep);
+		return objSeller;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getNString("DepName"));
+		return dep;
 	}
 
 	@Override
